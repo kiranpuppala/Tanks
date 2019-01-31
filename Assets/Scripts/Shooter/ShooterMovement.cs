5beecb0e7b1e2a1556469ccb4d220928bbd5537d
kiranpuppala_juspay <kiran.puppala@juspay.in>
2019-01-31 22:32:10 +0530

    final commit

---
+++ Assets/Scripts/Shooter/ShooterMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShooterMovement : MonoBehaviour {
+
+	 Transform player;
+//	PlayerHealth playerHealth;
+//	EnemyHealth enemyHealth;
+	UnityEngine.AI.NavMeshAgent nav;
+	public Rigidbody bullet;
+	public Transform gunTransform;
+	public float bulletForce;
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("PlayerTank").transform;
+//		playerHealth = player.GetComponent <PlayerHealth> ();
+//		enemyHealth = GetComponent <EnemyHealth> ();
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+	}
+
+
+	void Update ()
+	{
+//		Debug.Log ("PLAYER UPDATE");
+//		if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+//		{
+
+
+//		Vector3 offSetVector = new Vector3 (gunTransform.position.x, gunTransform.position.y, gunTransform.position.z);
+
+		if (nav.remainingDistance <= nav.stoppingDistance) {
+			
+			Rigidbody m_Bullet = Instantiate (bullet, gunTransform.position, gunTransform.rotation) as Rigidbody;
+			m_Bullet.velocity = bulletForce * gunTransform.forward;
+
+		}
+
+		nav.SetDestination (player.position);
+//		}
+//		else
+//		{
+//			nav.enabled = false;
+//		}
+
+	}
+}
---
+++ Assets/Scripts/Tank/TurretMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretMovement : MonoBehaviour {
+	float TurretTurn;
+	string move_name;
+	public int playerNum;
+	// Use this for initialization
+	void Start () {
+		move_name = "Lift"+playerNum;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		TurretTurn = Input.GetAxis (move_name);
+
+	}
+
+	private void FixedUpdate ()
+	{	
+		if ((transform.rotation.x < 0.13 && transform.rotation.x > -0.13) || (transform.rotation.x > 0.13 && TurretTurn < 0) || (transform.rotation.x < 0.13 && TurretTurn > 0)) {
+			Vector3 angle = new Vector3 (TurretTurn * Time.deltaTime * 50f, 0f, 0f);
+			transform.Rotate (angle);
+		}
+	}
+}
